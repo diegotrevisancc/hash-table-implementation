0f627feae9f4a7f71f097f00cfc13f183cdd9819
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-08-31 21:26:00 -0300

    first commit

---
+++ AgendaHeroes.py
@@ -0,0 +1,51 @@
+class AgendaHeroes:
+  def __init__(self):
+    self.size = 26
+    self.table = [None] * self.size
+    self.totalItems = 0
+
+  def hash_function(self, key):
+    indexFirstChar = int(((ord(key[0].lower()) / 98) - 1) * 100) + 1
+    print(indexFirstChar)
+    print("index: {} - value: {}".format(indexFirstChar, key))
+    return indexFirstChar
+
+  def insert(self, key):
+    index = self.hash_function(key)
+    if self.table[index] is None:
+      self.table[index] = key
+    else:
+      while self.table[index] is not None:
+        index += 1
+        if index == self.size:
+          index = 0
+      self.table[index] = key
+
+    self.totalItems += 1
+    return True
+
+  def search(self, key):
+    index = self.hash_function(key)
+    originalIndex = index
+
+    while self.table[index] is not None:
+      if self.table[index] == key:
+        return self.table[index]
+      index += 1
+      if index == self.size:
+        index = 0
+      if index == originalIndex:
+        return None
+
+    return None
+
+
+hash_table = AgendaHeroes()
+hash_table.insert("aa")
+hash_table.insert("b")
+hash_table.insert("z")
+print(hash_table.table)
+
+print(hash_table.search("aa"))
+print(hash_table.search("b"))
+print(hash_table.search("z"))
